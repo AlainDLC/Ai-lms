f56ed863e7fada132a57be99a57f1239245db2fd
AlainDLC <jd_alain@hotmail.com>
2025-02-02 20:15:12 +0100

    added new image and some css styling

--- ai-story/app/create-story/_components/StoryType.tsx
+++ ai-story/app/create-story/_components/StoryType.tsx
@@ -1,37 +1,49 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
-// lägg in bilder
 function StoryType() {
   const OptionList = [
     {
       label: "Story Book",
-      ImageUrl: "",
+      ImageUrl: "/storybook.png",
       isFree: true,
     },
     {
       label: "Bed Story",
-      ImageUrl: "",
+      ImageUrl: "/pilot.png",
       isFree: true,
     },
     {
       label: "Educational",
-      ImageUrl: "",
+      ImageUrl: "/ed.png",
       isFree: true,
     },
   ];
+
+  const [selecteOption, setSelectedOption] = useState<string>();
   return (
     <div>
-      <label className="font-bold text-4xl text-primary">2. Story Type</label>
-      <div>
+      <label className="font-bold text-4xl text-primary  ">2. Story Type</label>
+      <div className="grid grid-cols-3 gap-4 mt-5  ">
         {OptionList.map((item, index) => (
-          <div>
+          <div
+            className={`relative grayscale hover:grayscale-0 cursor-pointer  ${
+              selecteOption === item.label
+                ? "grayscale-0 border-5 rounded-3xl border-blue-300"
+                : "grayscale"
+            } `}
+            onClick={() => setSelectedOption(item.label)}
+          >
+            <h2 className="absolute bottom-6 text-center w-full text-primary-50 text-2xl ">
+              {item.label}
+            </h2>
             <Image
               src={item.ImageUrl}
               alt={item.label}
               width={300}
               height={500}
-              className="object-cover h-[260px] rounded-3xl"
+              className="object-cover h-full rounded-3xl "
             />
           </div>
         ))}
