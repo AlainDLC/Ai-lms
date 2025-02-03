afeb15500aa0a7eed84ee3f49c12caa079c13983
AlainDLC <jd_alain@hotmail.com>
2025-02-03 14:03:02 +0100

    backend schema implement

---
+++ ai-story/config/db.tsx
@@ -0,0 +1,9 @@
+import "./envConfig.ts";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL!);
+
+console.log(sql);
+export const db = drizzle(sql, { schema });
---
+++ ai-story/drizzle.config.ts
@@ -0,0 +1,10 @@
+import "./envConfig.ts";
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./config/schema.tsx",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DATABASE_URL!,
+  },
+});
